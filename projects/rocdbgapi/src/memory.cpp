4ef4232e8d23786976c4ec6498618ea69da48413
Pedro Alves <pedro@palves.net>
2026-02-16 14:00:50 +0000

    memory cache: global memory => agent memory, fix FIXME
    "xfer_global_memory" is a remnant of the Linux-only time where the
    cache was dealing with global addresses.  Even if yes, from the GPU
    perspective it is called "global", it is not necessarily global in the
    "unified global" sense.
    
    Since the memory cache is now specialized to agent_address_t, rename:
    
     xfer_global_memory => xfer_agent_memory
     read_global_memory => read_agent_memory
     write_global_memory => write_agent_memory
    
    Pass the agent to the memory_cache_t ctor, so we can use it in the
    memory_access_error_t throw in memory_cache_t::write_back, fixing a
    FIXME: instead of using address_space_t::global(), use the agent's
    address space.
    
    This also fixes a compilation error when compiling with GCC at -O2:
    
      In file included from /home/pedalves/rocm/dbgapi/src/src/memory.cpp:21:
      In member function ‘T amd::dbgapi::detail::base_address_t<T>::operator-(U) const [with U = amd::dbgapi::agent_address_t; T = amd::dbgapi::agent_address_t]’,
          inlined from ‘size_t amd::dbgapi::memory_cache_t::xfer_agent_memory(amd::dbgapi::agent_address_t, void*, const void*, size_t)’ at /home/pedalves/rocm/dbgapi/src/src/memory.cpp:678:32:
      /home/pedalves/rocm/dbgapi/src/src/memory.h:85:15: error: ‘<unnamed>.amd::dbgapi::agent_address_t::<unnamed>.amd::dbgapi::detail::base_address_t<amd::dbgapi::agent_address_t>::m_address’ may be used uninitialized [-Werror=maybe-uninitialized]
         85 |     return T{ m_address - decrement };
    	|               ^~~~~~~~~
      /home/pedalves/rocm/dbgapi/src/src/memory.cpp: In member function ‘size_t amd::dbgapi::memory_cache_t::xfer_agent_memory(amd::dbgapi::agent_address_t, void*, const void*, size_t)’:
      /home/pedalves/rocm/dbgapi/src/src/memory.cpp:678:28: note: ‘<anonymous>’ declared here
        678 |     size = agent_address_t{} - address;
    	|                            ^
    
    That's because of:
    
      base_address_t () = default;  => default-initializes members.
      uint64_t m_address;           => no initializer => left uninitialized.
    
    This is fixed in this commit because the "agent_address_t{}" simply
    goes away.
    
    Suggested-By: Lancelot SIX <lancelot.six@amd.com>
    Change-Id: Ib93ea39dcb9648a509a80c1dbd9f8bf79905587c

--- projects/rocdbgapi/src/memory.cpp
+++ projects/rocdbgapi/src/memory.cpp
@@ -726,8 +726,8 @@ memory_cache_t::prefetch (agent_address_t address, amd_dbgapi_size_t size)
 
   try
     {
-      m_xfer_global_memory (cache_line_begin, &staging_buffer[0], nullptr,
-                            cache_line_end - cache_line_begin);
+      m_xfer_agent_memory (cache_line_begin, &staging_buffer[0], nullptr,
+                           cache_line_end - cache_line_begin);
     }
   catch (const memory_error_t &)
     {
@@ -813,13 +813,12 @@ memory_cache_t::write_back (agent_address_t address, amd_dbgapi_size_t size)
 
       try
         {
-          size_t xfer_size = m_xfer_global_memory (
+          size_t xfer_size = m_xfer_agent_memory (
             cache_line_address, nullptr, &staging_buffer[0], request_size);
 
           if (xfer_size != request_size)
-            throw memory_access_error_t (
-              /* FIXME_lmoriche:  */
-              address_space_t::global (), cache_line_address + xfer_size);
+            throw memory_access_error_t (m_agent.agent_address_space (),
+                                         cache_line_address + xfer_size);
         }
       catch (const process_exited_exception_t &)
         {
@@ -862,15 +861,23 @@ memory_cache_t::discard (agent_address_t address, amd_dbgapi_size_t size,
 }
 
 size_t
-memory_cache_t::xfer_global_memory (agent_address_t address, void *read,
-                                    const void *write, size_t size)
+memory_cache_t::xfer_agent_memory (agent_address_t address, void *read,
+                                   const void *write, size_t size)
 {
   if (size == 0)
     return 0;
 
-  /* Clamp to the end of the global address space.  */
-  if (address > (address + size))
-    size = agent_address_t{} - address;
+  /* Clamp to the end of the agent address space.  */
+  auto max = m_agent.agent_address_space ().last_address ();
+  if (address > max)
+    throw memory_access_error_t (m_agent.agent_address_space (), address);
+  /* Number of bytes available from ADDRESS to MAX inclusive, minus
+     one.  MAX is the last representable address, so the full byte
+     count (MAX - ADDRESS + 1) wraps to zero when ADDRESS is 0.  Using
+     the "minus one" form avoids that wrap.  */
+  auto available_minus_one = max - address;
+  if (size - 1 > available_minus_one)
+    size = available_minus_one + 1;
 
   auto first_line = utils::align_down (address, cache_line_size);
   auto last_line = utils::align_down (address + size - 1, cache_line_size);
@@ -881,7 +888,7 @@ memory_cache_t::xfer_global_memory (agent_address_t address, void *read,
 
   /* If there are no cache lines affected by this access.  */
   if (begin == end)
-    return m_xfer_global_memory (address, read, write, size);
+    return m_xfer_agent_memory (address, read, write, size);
 
   /* For cached accesses, handle one cache line at a time.  */
   if (first_line != last_line)
@@ -892,7 +899,7 @@ memory_cache_t::xfer_global_memory (agent_address_t address, void *read,
           auto limit = utils::align_up (ptr + 1, cache_line_size);
           auto request_size = std::min (limit, ptr + size) - ptr;
 
-          auto xfer_size = xfer_global_memory (ptr, read, write, request_size);
+          auto xfer_size = xfer_agent_memory (ptr, read, write, request_size);
 
           ptr += xfer_size;
           if (read != nullptr)
